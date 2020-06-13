d1a372291d322f125e1e85e7a49bc89c9860136b
JonathanWoodell <woodell.jonathan@gmail.com>
2020-06-13 00:00:51 -0500

    Removed unneeded comments.

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ import json
 
 def csv_example(mongo_address, mongo_port, db_name, collection_name, csv_location):
     ingester = Mongo_Csv_Pandas_Data_Ingester(mongo_address=mongo_address, mongo_port=mongo_port, db_name=db_name, collection_name=collection_name)
-    # ingester.ingest_data(data_frame=ingester.create_data_frame(data_folder="data/table_P3/sumLevel_140/*.csv"))
     ingester.ingest_data(data_frame=ingester.create_data_frame(data_folder=csv_location))
 
 
 def dask_example(mongo_address, mongo_port, db_name, collection_name, csv_location):
-    # ingester = Mongo_Dask_Data_Ingester(mongo_address="127.0.0.1", mongo_port=27017, db_name="Data_Ingester", collection_name="Table_P3-sumLevel_140")
     ingester = Mongo_Dask_Data_Ingester(mongo_address=mongo_address, mongo_port=mongo_port, db_name=db_name, collection_name=collection_name)
-    # df = dd.read_csv("data/table_P3/sumLevel_140/*.csv")
     df = dd.read_csv(csv_location)
     ingester.ingest_data(data_frame=df)
 
